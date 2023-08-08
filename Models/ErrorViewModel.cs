6635545a11e1b7c33ea3edf98863d84eea688bd2
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2023-08-08 20:33:19 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ILibrary_BuyBooksOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
