65e5f62be851184aaf8c110f59f58d934c5b9829
IMRAN AL MUNYEEM <45540113+imranalmunyeem@users.noreply.github.com>
2023-08-09 19:22:58 +0100

    Category: Controller, View

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -15,8 +15,94 @@ namespace ILibrary_BuyBooksOnline.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList(); //Will retrieve the category data
-           
+
             return View(objCategoryList);
         }
+
+        //-------------------------------------------Create Controller------------------------------------------------//
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        //-------------------------------------------Edit Controller------------------------------------------------//
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+           
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category was updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        //-------------------------------------------Delete Controller------------------------------------------------//
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category was deleted successfully";
+            return RedirectToAction("Index");
+        }
+
     }
-}
+    }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ILibrary_BuyBooksOnline.Models
 {
     public class Category
     {
-        [Key]  // Data notation to let know below Id is the primary key
+
+        [Key]           // Data notation to let know below Id is the primary key
         public int Id { get; set; }
-        [Required] // Indicate that a specific property is required to have a non-null or non-empty value
+        [Required]      // Indicate that a specific property is required to have a non-null or non-empty value
+        [MaxLength(50)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]   //It will separate the Display from Order keyword
+        [Range(10, 100, ErrorMessage = "Display Order must be between 10-100")]
         public int DisplayOrder { get; set; }
+
+
     }
 }
